bebe608a898fdb1f6a25d5363101020dcc70e92f
topt-ai <ricardoacevedo1199@gmail.com>
2026-03-12 19:01:03 -0600

    chore: revert back to custom widget.js framework mapping

--- tuwebsv (1)/src/App.tsx
+++ tuwebsv (1)/src/App.tsx
@@ -6,32 +6,26 @@ import GoogleMetaAds from './pages/GoogleMetaAds';
 import Nosotros from './pages/Nosotros';
 import Contacto from './pages/Contacto';
 import Politicas from './pages/Politicas';
-import ChatWidget from './components/ChatWidget';
 
 export default function App() {
   const path = window.location.pathname;
-  let Content;
-  
   if (path === '/seo-local') {
-    Content = <SeoLocal />;
-  } else if (path === '/paginas-web') {
-    Content = <PaginasWeb />;
-  } else if (path === '/google-meta-ads') {
-    Content = <GoogleMetaAds />;
-  } else if (path === '/nosotros') {
-    Content = <Nosotros />;
-  } else if (path === '/contacto') {
-    Content = <Contacto />;
-  } else if (path === '/politicas') {
-    Content = <Politicas />;
-  } else {
-    Content = <Home />;
+    return <SeoLocal />;
   }
-
-  return (
-    <>
-      <ChatWidget />
-      {Content}
-    </>
-  );
+  if (path === '/paginas-web') {
+    return <PaginasWeb />;
+  }
+  if (path === '/google-meta-ads') {
+    return <GoogleMetaAds />;
+  }
+  if (path === '/nosotros') {
+    return <Nosotros />;
+  }
+  if (path === '/contacto') {
+    return <Contacto />;
+  }
+  if (path === '/politicas') {
+    return <Politicas />;
+  }
+  return <Home />;
 }
